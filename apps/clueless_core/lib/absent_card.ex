b7e768b1d30c18f2032d85852bd47f991294b494
Jacopo Zagoli <poipoi.jacopo@hotmail.com>
2025-05-08 15:57:34 +0200

    add set of cards to list of players absent cards set

--- apps/clueless_core/lib/absent_card.ex
+++ apps/clueless_core/lib/absent_card.ex
@@ -4,45 +4,65 @@ defmodule CluelessCore.AbsentCard do
   """
 
   @doc """
-    Adds a card to the set of absent cards for a player or a list of players.
+    Adds a card or set of cards to the set of absent cards for a player or a list of players.
 
     ## Examples
 
       iex> absent_cards = %{1 => MapSet.new([:knife])}
       iex> absent_cards = add_card_to_absent(absent_cards, 1, :garage)
-      iex> Enum.count(Map.get(absent_cards, 1))
-      2
+      iex> Map.get(absent_cards, 1)
+      MapSet.new([:knife, :garage])
 
       iex> absent_cards = add_card_to_absent(%{}, 2, :knife)
-      iex> Enum.count(Map.get(absent_cards, 2))
-      1
+      iex> Map.get(absent_cards, 2)
+      MapSet.new([:knife])
 
       iex> absent_cards = %{1 => MapSet.new([:knife])}
       iex> absent_cards = add_card_to_absent(absent_cards, [1, 2], :garage)
-      iex> Enum.count(Map.get(absent_cards, 1))
-      2
-      iex> Enum.count(Map.get(absent_cards, 2))
-      1
+      iex> Map.get(absent_cards, 1)
+      MapSet.new([:knife, :garage])
+      iex> Map.get(absent_cards, 2)
+      MapSet.new([:garage])
 
       iex> absent_cards = add_card_to_absent(%{}, [2], :knife)
-      iex> Enum.count(Map.get(absent_cards, 2))
-      1
+      iex> Map.get(absent_cards, 2)
+      MapSet.new([:knife])
 
       iex> absent_cards = add_card_to_absent(%{1 => MapSet.new([:knife])}, [], :garage)
-      iex> Enum.count(Map.get(absent_cards, 1))
-      1
+      iex> Map.get(absent_cards, 1)
+      MapSet.new([:knife])
 
       iex> absent_cards = add_card_to_absent(%{1 => MapSet.new([:knife])}, [2], :knife)
-      iex> Enum.count(Map.get(absent_cards, 1))
-      1
+      iex> Map.get(absent_cards, 1)
+      MapSet.new([:knife])
+
+      iex> absent_cards = add_card_to_absent(%{}, 1, MapSet.new([:garage, :knife]))
+      iex> Map.get(absent_cards, 1)
+      MapSet.new([:garage, :knife])
+
+      iex> absent_cards = add_card_to_absent(%{}, [1, 2], MapSet.new([:garage, :knife]))
+      iex> Map.get(absent_cards, 1)
+      MapSet.new([:garage, :knife])
+      iex> Map.get(absent_cards, 2)
+      MapSet.new([:garage, :knife])
+
   """
-  def add_card_to_absent(%{} = absent_cards, players, card) when is_list(players) do
+  def add_card_to_absent(absent_cards, players, %MapSet{} = cards)
+      when is_map(absent_cards) do
+    Enum.reduce(cards, absent_cards, fn card, absent_cards ->
+      add_card_to_absent(absent_cards, players, card)
+    end)
+  end
+
+  def add_card_to_absent(absent_cards, players, card)
+      when is_list(players) and is_map(absent_cards) do
     Enum.reduce(players, absent_cards, fn player, absent_cards ->
       add_card_to_absent(absent_cards, player, card)
     end)
   end
 
-  def add_card_to_absent(%{} = absent_cards, player, card) when is_integer(player) do
+  def add_card_to_absent(absent_cards, player, card)
+      when is_integer(player) and is_map(absent_cards) and is_atom(card) do
     {_, absent_cards} =
       Map.get_and_update(absent_cards, player, fn
         nil -> {nil, MapSet.new([card])}
