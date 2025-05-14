b58095a95f6dea5699e4a937f6466f16e8b2e0e4
Jacopo Zagoli <poipoi.jacopo@hotmail.com>
2025-05-14 11:48:28 +0200

    players names should stay in the client

--- apps/clueless_web/lib/clueless_web/controllers/new_game_controller.ex
+++ apps/clueless_web/lib/clueless_web/controllers/new_game_controller.ex
@@ -2,12 +2,11 @@ defmodule CluelessWeb.NewGameController do
   use CluelessWeb, :controller
   alias Clueless.ClueGame
 
-  def new_game(conn, %{"players" => players}) when is_list(players) do
-    game = ClueGame.new(Enum.count(players))
+  def new_game(conn, %{"players" => players_count}) when is_integer(players_count) do
+    game = ClueGame.new(players_count)
 
     conn
     |> fetch_session()
-    |> put_session(:players, players)
     |> put_session(:game, game)
     |> put_resp_content_type("text/plain")
     |> send_resp(201, "")
